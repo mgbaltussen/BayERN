6507765009d40f7f0e47b4646e3f3bdabd574617
Mathieu Baltussen <m.g.baltussen@gmail.com>
2022-01-11 11:52:22 +0100

    update package list

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     version="0.1.0",
     description="A Python package to perform Bayesian inference on enzyme kinetics data with PyMC3",
     long_description=readme,
+    packages=['bayern'],
     author="Mathieu G. Baltussen",
     author_email="m.g.baltussen@gmail.com",
     license=license,
-    packages=find_packages(exclude=('test', 'docs')),
 )
